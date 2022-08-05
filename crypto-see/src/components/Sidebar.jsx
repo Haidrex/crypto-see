7fb20d06f5337116ffb9793f91f8064199059dc4
Rytis <rytiskacinskis@gmail.com>
2022-08-05 19:12:10 +0300

    sidebar

---
+++ crypto-see/src/components/Sidebar.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import {
+	Box,
+	Drawer,
+	List,
+	Divider,
+	ListItem,
+	ListItemButton,
+	ListItemText,
+} from '@mui/material';
+
+const drawerWidth = 240;
+
+const Sidebar = ({ component }) => {
+	let navigate = useNavigate();
+
+	const handleClick = (path) => {
+		navigate({ path });
+	};
+	return (
+		<Box sx={{ display: 'flex' }}>
+			<Drawer
+				sx={{
+					width: drawerWidth,
+					flexShrink: 0,
+					'& .MuiDrawer-paper': {
+						width: drawerWidth,
+						boxSizing: 'border-box',
+						backgroundColor: 'primary.main',
+					},
+				}}
+				variant="permanent"
+				anchor="left"
+			>
+				<List>
+					<ListItem key="home" disablePadding>
+						<ListItemButton onClick={() => handleClick('/')}>
+							<ListItemText primary="Coins" sx={{ color: 'white' }} />
+						</ListItemButton>
+					</ListItem>
+					<Divider />
+					<ListItem key="exchanges" disablePadding>
+						<ListItemButton onClick={() => handleClick('/exchanges')}>
+							<ListItemText primary="Exchanges" sx={{ color: 'white' }} />
+						</ListItemButton>
+					</ListItem>
+					<Divider />
+				</List>
+			</Drawer>
+			{component}
+		</Box>
+	);
+};
+
+export default Sidebar;
